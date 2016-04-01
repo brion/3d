cc767883ac27e677a33ea8491c217be4f28949b0
Gilles Dubuc <gdubuc@wikimedia.org>
2016-04-01 18:16:44 +0300

    Boilerplate extension created with https://github.com/mooeypoo/MWStew

---
+++ Hooks.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 3d extension hooks
+ *
+ * @file
+ * @ingroup Extensions
+ * @license MIT
+ */
+class 3dHooks {
+
+
+}
---
+++ modules/ext.3d.js
@@ -0,0 +1,8 @@
+( function () {
+	/**
+	 * @class mw.3d
+	 * @singleton
+	 */
+	mw.3d = {
+	};
+}() );
